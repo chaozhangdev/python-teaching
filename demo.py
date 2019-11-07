2c4c6abf2bc799c64ab028f2d03c6423b701a51f
Chao Zhang <raidenchao@gmail.com>
2019-11-06 21:24:53 -0800

    upload

---
+++ demo.py
@@ -0,0 +1,7 @@
+import turtle
+t = turtle.Pen()
+t.speed(0)
+t.pencolor("blue")
+for i in range(500):
+    t.forward(i)    
+    t.left(91)
---
+++ turtle.py
@@ -0,0 +1,5 @@
+import turtle
+t = turtle.Pen()
+for i in range(100):
+    t.forward(i)
+    t.left(90)
